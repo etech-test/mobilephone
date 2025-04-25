b0eed6a60694f131147212077f4f747ce2990417
Etech-Amos <oyelamiamos@yahoo.com>
2025-04-25 00:33:52 -0500

    logic added

---
+++ subration.sh
@@ -0,0 +1,10 @@
+script to analysed special variables
+#1/bin/bash
+
+echo "File Name: $0"
+echo "First Parameter : $1"
+echo "Second Parameter : $2"
+echo "Second Parameter :$3"
+echo "Quoted values":$@
+echo "quotedvalues":$#
+echo "Total Number of Parameters :$#
